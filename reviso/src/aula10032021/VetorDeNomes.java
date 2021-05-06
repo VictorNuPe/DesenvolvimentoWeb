efcfc4750e98b0850ba38838e0ba709268899b7b
Viguiwar <victor.np8@gmail.com>
2021-05-05 22:12:23 -0300

    O commit prometido

--- reviso/src/aula10032021/VetorDeNomes.java
+++ reviso/src/aula10032021/VetorDeNomes.java
@@ -1,29 +1,10 @@
 package aula10032021;
 
+
 public class VetorDeNomes {
 
-	public static void main(String[] args) {
-		
-		String[] nomes = new String[3];
-		nomes[0] = "João";
-		nomes[1] = "Maria";
-		nomes[2] = "Pedro";
-		
-		
-		System.out.println("Nome: " + nomes[0]);
-		System.out.println("Nome: " + nomes[1]);
-		System.out.println("Nome: " + nomes[2]);
-		
-		for (int i = 0; i < nomes.length; i++) {
-			System.out.println("Nome: " + nomes[i]);
-		}
-		
-		System.out.println();
-		
-		for(String nome:nomes) {
-			System.out.println("Nome: " + nome);
-		}
-		
-	}
+	double a = 10;
+	double b = 5;
+	double c = a/b;
 
 }
